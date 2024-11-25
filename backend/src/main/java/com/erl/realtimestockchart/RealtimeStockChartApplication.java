a891f5eeda6e2d02cfafc130102834c6efa70726
since1909 <since1909@daum.net>
2024-11-25 15:56:14 +0900

    Add #7: init spring boot project
    add:
    index.html
    application.yml
    
    fix:
    build.gradle

---
+++ backend/src/main/java/com/erl/realtimestockchart/RealtimeStockChartApplication.java
@@ -0,0 +1,13 @@
+package com.erl.realtimestockchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeStockChartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeStockChartApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/erl/realtimestockchart/RealtimeStockChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erl.realtimestockchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeStockChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
